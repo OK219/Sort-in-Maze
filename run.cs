a563e573a7946d940e506f4eb641119c79cb4fbb
OK <143250301+OK219@users.noreply.github.com>
2025-10-27 12:27:44 +0500

    решение через а стар

--- run.cs
+++ run.cs
@@ -22,12 +22,16 @@ class Program
     static int Solve(List<string> lines)
     {
         var initialState = GetInitialState(lines);
-        var result = AStar(initialState);
+        //var result = BruteForce(initialState);
+        var result = RealAStar(initialState);
         return result;
     }
 
     static State GetInitialState(List<string> lines)
     {
+        lines = lines.Skip(2)
+            .Take(lines.Count - 3)
+            .ToList();
         var hall = "...........".ToCharArray();
         var rooms = new char[4].Select(x => new char[lines.Count]).ToArray();
         for (var i = 0; i < lines.Count; i++)
@@ -61,7 +65,7 @@ class Program
     {
         var start = Math.Min(initialPos, targetPos);
         var end = Math.Max(initialPos, targetPos);
-        state.Deconstruct(out var hall);
+        state.GetHall(out var hall);
         if (hall[initialPos] == '.') throw new Exception("Can't move nothing");
         for (var i = start; i <= end; i++)
         {
@@ -104,8 +108,7 @@ class Program
 
     static bool CanPut(State state, int hallPos)
     {
-        return //RoomMapInt[hallPos] == state.Hall[hallPos] &&
-            state.Rooms[(hallPos - 2) / 2].All(x => x == '.' || x == RoomMapInt[hallPos]);
+        return state.Rooms[(hallPos - 2) / 2].All(x => x == '.' || x == RoomMapInt[hallPos]);
     }
 
     static bool CanMove(State state, int initialPos, int targetPos)
@@ -125,7 +128,7 @@ class Program
 
     static bool IsCompleted(State state)
     {
-        return !state.Rooms.Where((t, j) => t.Any(x => x != RoomMapInt[j * 2 + 2])).Any();
+        return !state.Rooms.Where((room, j) => room.Any(x => x != RoomMapInt[j * 2 + 2])).Any();
     }
 
     static (State state, int Weight) MoveAndPut(State state, int weight, int hallPos)
@@ -136,18 +139,108 @@ class Program
         return (putState, weight + moveWeight + putWeight);
     }
 
-    static int AStar(State initialState)
+    static int RealAStar(State initialState)
+    {
+        var q = new PriorityQueue<State, int>();
+        var weights = new Dictionary<State, int>
+        {
+            [initialState] = 0
+        };
+        var initialWeight = H(initialState);
+        q.Enqueue(initialState, initialWeight);
+        while (q.Count > 0)
+        {
+            var state = q.Dequeue();
+            if (IsCompleted(state))
+                return weights[state];
+            var states = GetPossibleStates(state);
+            foreach (var (nextState, nextWeight) in states)
+            {
+                var newWeight = weights[state] + nextWeight;
+                if (weights.TryGetValue(nextState, out var value) && newWeight >= value) continue;
+                weights[nextState] = newWeight;
+                q.Enqueue(nextState, newWeight + H(nextState));
+            }
+        }
+
+        return -1;
+    }
+
+    static List<(State state, int Weight)> GetPossibleStates(State state)
+    {
+        var states = new List<(State, int)>();
+        for (var i = 0; i < state.Hall.Length; i++)
+        {
+            if (state.Hall[i] == '.') continue;
+            var targetPos = RoomMapChar[state.Hall[i]];
+            if (!(CanMove(state, i, targetPos) && CanPut(state, targetPos))) continue;
+            var (putState, nextWeight) = MoveAndPut(state, 0, i);
+            states.Add((putState, nextWeight));
+        }
+
+        for (var i = 0; i < state.Rooms.Length; i++)
+        {
+            if (!CanTake(state, i)) continue;
+            var (takeState, takeWeight) = TakeFromRoom(state, i);
+            var hallPos = i * 2 + 2;
+            if (CanMove(state, hallPos, RoomMapChar[takeState.Hall[hallPos]]) &&
+                CanPut(state, RoomMapChar[takeState.Hall[hallPos]]))
+            {
+                var (putState, nextWeight) = MoveAndPut(takeState, 0 + takeWeight, hallPos);
+                states.Add((putState, nextWeight));
+            }
+            else
+            {
+                for (var j = 0; j < takeState.Hall.Length; j++)
+                {
+                    if (RoomMapInt.ContainsKey(j) || !CanMove(takeState, hallPos, j)) continue;
+                    var (moveState, moveWeight) = MoveInHall(takeState, hallPos, j);
+                    states.Add((moveState, 0 + takeWeight + moveWeight));
+                }
+            }
+        }
+
+        return states;
+    }
+
+    static int H(State state)
+    {
+        var weight = 0;
+        for (var i = 0; i < state.Hall.Length; i++)
+        {
+            if (state.Hall[i] == '.') continue;
+            var roomPos = RoomMapChar[state.Hall[i]];
+            weight += Math.Abs(roomPos - i) * Weights[state.Hall[i]];
+        }
+
+        for (var i = 0; i < state.Rooms.Length; i++)
+        {
+            for (var j = 0; j < state.Rooms[i].Length; j++)
+            {
+                var character = state.Rooms[i][j];
+                if (character == '.') continue;
+                var roomPos = RoomMapChar[character];
+                weight += (Math.Abs(roomPos - (i * 2 + 2)) + i + 1) * Weights[character];
+            }
+        }
+
+        return weight;
+    }
+
+    static int BruteForce(State initialState)
     {
         var q = new PriorityQueue<(State State, int Weight), int>();
+        var visited = new HashSet<State>();
         q.Enqueue((initialState, 0), 0);
         while (q.Count > 0)
         {
             var shouldBreak = false;
             var (state, weight) = q.Dequeue();
+            if (!visited.Add(state)) continue;
             if (IsCompleted(state)) return weight;
             for (var i = 0; i < state.Hall.Length; i++)
             {
-                if (state.Hall[i] == '.') continue; 
+                if (state.Hall[i] == '.') continue;
                 var targetPos = RoomMapChar[state.Hall[i]];
                 if (!(CanMove(state, i, targetPos) && CanPut(state, targetPos))) continue;
                 var (putState, nextWeight) = MoveAndPut(state, weight, i);
@@ -166,6 +259,7 @@ class Program
                     CanPut(state, RoomMapChar[takeState.Hall[hallPos]]))
                 {
                     var (putState, nextWeight) = MoveAndPut(takeState, weight + takeWeight, hallPos);
+                    if (visited.Contains(putState)) continue;
                     q.Enqueue((putState, nextWeight), nextWeight);
                 }
                 else
@@ -174,6 +268,7 @@ class Program
                     {
                         if (RoomMapInt.ContainsKey(j) || !CanMove(takeState, hallPos, j)) continue;
                         var (moveState, moveWeight) = MoveInHall(takeState, hallPos, j);
+                        if (visited.Contains(moveState)) continue;
                         q.Enqueue((moveState, weight + takeWeight + moveWeight),
                             weight + takeWeight + moveWeight);
                     }
@@ -188,24 +283,56 @@ class Program
     {
         var lines = new List<string>();
         string line;
-
         while (!string.IsNullOrEmpty(line = Console.ReadLine()))
         {
             lines.Add(line);
         }
 
-        var result = Solve(lines
-            .Skip(2)
-            .Take(lines.Count - 3)
-            .ToList());
+        var result = Solve(lines);
         Console.WriteLine(result);
     }
 }
 
 public class State(char[] hall, char[][] rooms)
 {
-    public char[] Hall = hall;
-    public char[][] Rooms = rooms;
+    protected bool Equals(State other)
+    {
+        for (var i = 0; i < Hall.Length; i++)
+        {
+            if (Hall[i] != other.Hall[i]) return false;
+        }
+
+        for (var i = 0; i < Rooms.Length; i++)
+        {
+            for (var j = 0; j < Rooms[i].Length; j++)
+            {
+                if (Rooms[i][j] != other.Rooms[i][j]) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((State)obj);
+    }
+
+    public static bool operator ==(State? left, State? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(State? left, State? right)
+    {
+        return !Equals(left, right);
+    }
+
+    public readonly char[] Hall = hall;
+    public readonly char[][] Rooms = rooms;
 
     public void Deconstruct(out char[] hall, out char[][] rooms)
     {
@@ -213,8 +340,15 @@ public class State(char[] hall, char[][] rooms)
         rooms = Rooms.Select(x => x.ToArray()).ToArray();
     }
 
-    public void Deconstruct(out char[] hall)
+    public void GetHall(out char[] hall)
     {
         hall = Hall.ToArray();
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = Hall.Aggregate(0, HashCode.Combine);
+        hash = Rooms.Aggregate(hash, (cur, room) => room.Aggregate(cur, HashCode.Combine));
+        return hash;
     }
 }
